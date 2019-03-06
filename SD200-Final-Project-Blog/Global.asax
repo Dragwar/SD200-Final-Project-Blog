95c594474201ca107762275e88f4ab8d4b95f71d
Everett Grassler <dragwar9@gmail.com>
2019-03-06 15:07:32 -0600

    Add project files.

---
+++ SD200-Final-Project-Blog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SD200_Final_Project_Blog.MvcApplication" Language="C#" %>
---
+++ SD200-Final-Project-Blog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SD200_Final_Project_Blog.Startup))]
+namespace SD200_Final_Project_Blog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
