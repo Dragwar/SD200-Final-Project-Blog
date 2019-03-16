5c899177f94adf460fa74daf036b57d316b5b6e0
Everett Grassler <dragwar9@gmail.com>
2019-03-16 03:25:45 -0500

    Added simple submit prevention on empty search

---
+++ SD200-Final-Project-Blog/Scripts/myScripts/preventFormSubmitOnEmptySearch.js
@@ -0,0 +1,8 @@
+﻿document.querySelector("#searchForm").addEventListener("submit", (e) => {
+    const search = document.querySelector("#searchForm").firstElementChild.firstElementChild.value.trim();
+    if (search === null || search === "" || search === undefined) {
+        e.preventDefault();
+        console.warn(`You can't search with no string input or whitespace`);
+    }
+    console.log(search);
+});
