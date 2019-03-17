c4d9f15aa65950d11075f3e9c4d9ad83a232ed83
Everett Grassler <dragwar9@gmail.com>
2019-03-16 20:47:33 -0500

    Got basic hard coded comment template showing

--- SD200-Final-Project-Blog/Controllers/AccountController.cs
+++ SD200-Final-Project-Blog/Controllers/AccountController.cs
@@ -79,7 +79,7 @@ namespace SD200_Final_Project_Blog.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return Redirect(returnUrl);
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
